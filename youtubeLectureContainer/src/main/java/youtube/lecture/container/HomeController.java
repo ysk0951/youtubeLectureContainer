cfa23cdf765edfe99f5c9d003a09cd0d21076b22
yskimWEB <abcm556@naver.com>
2020-07-06 11:24:10 +0900

    #setting

--- youtubeLectureContainer/src/main/java/youtube/lecture/container/HomeController.java
+++ youtubeLectureContainer/src/main/java/youtube/lecture/container/HomeController.java
@@ -25,6 +25,7 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Setting... YOUTUBE CONTAINER", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);

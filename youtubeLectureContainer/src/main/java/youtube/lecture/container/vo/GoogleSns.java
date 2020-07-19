0dd1c99375281066d057ba844b9b2e29956919e6
yskim-web <abcm556@naver.com>
2020-07-19 14:27:47 +0900

    #

--- youtubeLectureContainer/src/main/java/youtube/lecture/container/vo/GoogleSns.java
+++ youtubeLectureContainer/src/main/java/youtube/lecture/container/vo/GoogleSns.java
@@ -3,7 +3,9 @@ package youtube.lecture.container.vo;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 
 public class GoogleSns {
-	private Google googleClientID;
+	
+	private String google;
+	private String googleClientID;
 	private GoogleClientSecrets googleClientSecret;
-	private GoogleRedirectUrl googleRedirectUrl;
+	private String googleRedirectUrl;
 }

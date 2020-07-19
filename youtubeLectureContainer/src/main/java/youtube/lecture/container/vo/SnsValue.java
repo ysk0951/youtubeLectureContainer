aabfbef4524d958f34685d9c5237c1d588b97972
yskim-web <abcm556@naver.com>
2020-07-19 14:36:04 +0900

    ## Rombok 설치 필요

---
+++ youtubeLectureContainer/src/main/java/youtube/lecture/container/vo/SnsValue.java
@@ -0,0 +1,14 @@
+package youtube.lecture.container.vo;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+
+
+public class SnsValue {
+
+	private String service;
+	private String clientID;
+	private String clientSecret;
+	private String rdirectUrl;
+	
+	
+}

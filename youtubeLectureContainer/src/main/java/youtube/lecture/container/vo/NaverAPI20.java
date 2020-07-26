1f2d75c931441ec5b5e0877fc6c3b120f9592fe9
yskim-web <abcm556@naver.com>
2020-07-26 18:16:39 +0900

    #

--- youtubeLectureContainer/src/main/java/youtube/lecture/container/vo/NaverAPI20.java
+++ youtubeLectureContainer/src/main/java/youtube/lecture/container/vo/NaverAPI20.java
@@ -7,10 +7,10 @@ public class NaverAPI20 extends DefaultApi20 implements SnsUrls {
 	private NaverAPI20() {}
 	private static NaverAPI20 _instance;
 	public static NaverAPI20 getInstance() {
-		if(_instance == null)
-			return new NaverAPI20();
-		else 
-			return _instance;
+		if(_instance == null) {
+			_instance = new NaverAPI20();
+		}
+		return _instance;
 	}
 	public static class InstanceHolder{
 		

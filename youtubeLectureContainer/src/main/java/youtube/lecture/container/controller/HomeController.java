0b372577e5a6820c2324c7721d6027a69efc3515
yskimWEB <abcm556@naver.com>
2020-07-27 10:40:51 +0900

    #

--- youtubeLectureContainer/src/main/java/youtube/lecture/container/controller/HomeController.java
+++ youtubeLectureContainer/src/main/java/youtube/lecture/container/controller/HomeController.java
@@ -3,48 +3,24 @@ package youtube.lecture.container.controller;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-import java.util.Map;
-
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
-import org.springframework.social.google.connect.GoogleOAuth2Template;
-import org.springframework.social.oauth1.OAuth1Parameters;
 import org.springframework.social.oauth2.GrantType;
 import org.springframework.social.oauth2.OAuth2Operations;
 import org.springframework.social.oauth2.OAuth2Parameters;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.common.util.concurrent.ExecutionError;
-
-import youtube.lecture.container.vo.AuthInfo;
 import youtube.lecture.container.vo.SNSLogin;
 import youtube.lecture.container.vo.SnsValue;
 
 @Controller
 public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	@Inject
-	private AuthInfo authInfo;
 	@Inject
 	private SnsValue naverSns;
 	@Inject
@@ -54,17 +30,6 @@ public class HomeController {
 	@Inject
 	private OAuth2Parameters googleOauth2parameters;
 	
-	//App Engine identity Urlshortener ClassNotFound
-	//추후 공부 필요
-//	private static Urlshortener newUrlshortener() {
-//		  AppIdentityCredential credential =
-//		      new AppIdentityCredential(
-//		          Collections.singletonList(UrlshortenerScopes.URLSHORTENER));
-//		  return new Urlshortener.Builder(new UrlFetchTransport(),
-//		                                  JacksonFactory.getDefaultInstance(),
-//		                                  credential)
-//		      .build();
-//		}
 	//main
 	@RequestMapping(value = "/", method = {RequestMethod.GET , RequestMethod.POST})
 	public String join(Locale locale, Model model) {
@@ -76,45 +41,6 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		return "main";
 	}
-  @RequestMapping(value = "/googleSignInCallback")
-    public String doSessionAssignActionPage(HttpServletRequest request) throws Exception {
- 
-        String code = request.getParameter("code");
-        System.out.println(code);
-        //RestTemplate을 사용하여 Access Token 및 profile을 요청한다.
-        RestTemplate restTemplate = new RestTemplate();
-        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
-        parameters.add("code", code);
-        parameters.add("client_id", authInfo.getClientId());
-        parameters.add("client_secret", authInfo.getClientPw());
-       // parameters.add("redirect_uri", googleOAuth2Parameters.getRedirectUri());
-        parameters.add("grant_type", "authorization_code");
- 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<MultiValueMap<String, String>>(parameters, headers);
-        ResponseEntity<Map> responseEntity = restTemplate.exchange("https://www.googleapis.com/oauth2/v4/token", HttpMethod.POST, requestEntity, Map.class);
-        Map<String, Object> responseMap = responseEntity.getBody();
- 
-        // id_token 라는 키에 사용자가 정보가 존재한다.
-        // 받아온 결과는 JWT (Json Web Token) 형식으로 받아온다. 콤마 단위로 끊어서 첫 번째는 현 토큰에 대한 메타 정보, 두 번째는 우리가 필요한 내용이 존재한다.
-        // 세번째 부분에는 위변조를 방지하기 위한 특정 알고리즘으로 암호화되어 사이닝에 사용한다.
-        //Base 64로 인코딩 되어 있으므로 디코딩한다.
- 
-        String[] tokens = ((String)responseMap.get("id_token")).split("\\.");
-        Base64 base64 = new Base64(true);
-        String body = new String(base64.decode(tokens[1]));
-        
-        System.out.println(tokens.length);
-        System.out.println(new String(Base64.decodeBase64(tokens[0]), "utf-8"));
-        System.out.println(new String(Base64.decodeBase64(tokens[1]), "utf-8"));
- 
-        //Jackson을 사용한 JSON을 자바 Map 형식으로 변환
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, String> result = mapper.readValue(body, Map.class);
-        System.out.println(result.get(""));
-        return "redirect:/join";
-    }
 	
 	//error 404 page
 	@RequestMapping(value = "/error404", method = RequestMethod.GET)
@@ -127,7 +53,7 @@ public class HomeController {
 	
 	//senior coding 38:39
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public void login(Model model) throws Exception{
+	public String login(Model model) throws Exception{
 		logger.info("login GET ... ");
 		
 		//naver
@@ -135,11 +61,12 @@ public class HomeController {
 		model.addAttribute("naver_url", naverLogin.getNaverAuthURL());
 
 		//google-scribeJava
-		//SNSLogin googleLogin = new SNSLogin(googleSns);
-		//model.addAttribute("google_url", googleLogin.getGoogleAuthURL());
-		
+			//SNSLogin googleLogin = new SNSLogin(googleSns);
+			//model.addAttribute("google_url", googleLogin.getGoogleAuthURL());
 		//goole 다른방식
 		OAuth2Operations oauthOperations = googleConnectionFactory.getOAuthOperations();
 		oauthOperations.buildAuthenticateUrl(GrantType.AUTHORIZATION_CODE, googleOauth2parameters);
+		
+		return "main";
 	}
 }
--- youtubeLectureContainer/src/main/webapp/WEB-INF/views/main.jsp
+++ youtubeLectureContainer/src/main/webapp/WEB-INF/views/main.jsp
@@ -11,48 +11,20 @@
 	href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
 	integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
 	crossorigin="anonymous">
-<link href="/ylc/resources/css/main.css"" rel="stylesheet" type="text/css">
-
+<link href="/container/resources/css/main.css"" rel="stylesheet" type="text/css">
 </head>
 <body> 
-<h1 align="center">	YOUTUBE LECTURE CONTAINER </h1><br/>
-<h1 align="center">	https://joypinkgom.tistory.com/27?category=874360</h1><br/>
-<h1 align="center">	https://gdtbgl93.tistory.com/73</h1><br/>
-<!--
-	관리 탭 : https://console.cloud.google.com/apis/credentials?project=youtubelecturecontainer
-	라이브러리 도큐먼트 : https://developers.google.com/api-client-library/java/google-api-java-client/oauth2
-	Google OAuto 관련 : https://gdtbgl93.tistory.com/73
--->
+<h3 align="center">	YOUTUBE LECTURE CONTAINER </h1><br/>
 <table>
-	<tr>
-		<td>
-			LIST
-		</td>
-		<td>
-			STREAM
-		</td>
-	</tr>
-	<tr>
-		<td>
-		TESTAPI
-		</td>
+	<tr> 
 		<td>
 		 <a href="${google_url}">
 			 <button id="btnJoinGoogle" class="btn btn-primary btn-round" style="width: 100%; height: 30%">
               	<i class="fa fa-google" aria-hidden="true">
               		Google Login
               	</i>
-             </button>
-            </a> 
-
-		</td>
-	</tr>
-	<tr>
-		<td>
-			MY LIST 
-		</td>
-		<td>
-			<img src="/ylc/resources/css/java.png">
+             </button> 
+          </a> 
 		</td>
 	</tr>
 </table>
